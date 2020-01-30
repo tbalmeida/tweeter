9b8b321dfda92c1a72e69d96a944a639d6aba135
tbalmeida <tbalmeida@gmail.com>
2020-01-30 03:20:06 +0000

    Error messages added properly

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,6 +3,11 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
 
   // reference to the name of the tweets container on index.html  
   const $tweetContainer = $(".tweets");
@@ -13,12 +18,13 @@
     const firstDate = new Date(tweet.created_at);
     const secondDate = new Date();
     const diffDays = Math.round(Math.abs((firstDate - secondDate) / oneDay));
+    const safeHTML = `<p>${escape(tweet.content.text)}</p>`;
     const myTweet = `<article>` +
         `<header class="tweet">` +
         `<span><img src="${tweet.user.avatars}" width="80" height="80" align=center>${tweet.user.name}</span>` +
         `<span>${tweet.user.handle}</span>` +
         `</header>` +
-        `<p>${tweet.content.text}</p>` +
+        `<p>${safeHTML}</p>` +
         `<footer style="display: flex; justify-content: space-between; border-top: solid 2px darkgrey;">` +
         `<span>${diffDays} days ago (${firstDate.toDateString()})</span>` +
         `<span>[like] [comment]</span>` +
@@ -43,24 +49,37 @@ const getTweetsFromServer = function() {
   });
 }
 
-
 const tweetPostHandler = function () {
   $(function() {
     const $button = $("#postTweet");
 
     $button.on("click", function () {
       event.preventDefault();
-      const formData = $( "form" ).serialize();
 
-      $.ajax( {
-        url: "/tweets",
-        method: "POST",
-        data: formData
-      })
-      .then( function (createdTweet) {
-        getTweetsFromServer();
-      })
-
+      // Get the tweet text, trim the leading/trailing white spaces and validates the size
+      const myTweet = $("#myTweet").val();
+      myTweet.trim();
+      if (myTweet.length > 140){
+        $( "#errorMsg" ).slideDown( 300 );
+        $( "#errorMsg" ).text("Your tweet exceeded the maximum message length.");
+        alert("here");
+        return false;
+      } else if (myTweet.length === 0) {
+        $( "#errorMsg" ).slideDown( 300 );
+        $("#errorMsg").text("Your tweet can't be empty.");
+        return false;
+      } else {
+        const formData = $( "form" ).serialize();
+        $.ajax( {
+          url: "/tweets",
+          method: "POST",
+          data: formData
+        })
+        .then( function (createdTweet) {
+          getTweetsFromServer();
+        })
+        $( "#errorMsg" ).slideUp( 300 );
+      }
     });
   });
 };
@@ -68,3 +87,24 @@ const tweetPostHandler = function () {
 // attaches the new handler to the Tweet button
 tweetPostHandler();
 getTweetsFromServer();
+
+// hides the new tweet menu
+$( "#newTweetMenu" ).slideUp( 0 );
+
+// binds the function to toggle the visibility of the new tweet area to the "Write a new tweet" area
+$( ".writeNewTweet" ).bind( "click", function() {
+  $( "#newTweetMenu" ).slideToggle( 1200 );
+  $("#myTweet").focus();
+});
+
+
+// binds the function to toggle the visibility of the new tweet area to the "Write a new tweet" area
+$( ".arrowUp" ).bind( "click", function() {
+  $([document.documentElement, document.body]).animate({
+      scrollTop: $(".username").offset().top
+  }, 2000);
+  $( "#newTweetMenu" ).slideDown( 1200 );
+  $("#myTweet").focus();
+});
+
+$("#errorMsg" ).slideUp( 0 );
